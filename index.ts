e0f6311b5930456c7dcf40b8ee717291e6c7d169
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-03-09 23:20:50 +0200

    Sort

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 // source: https://github.com/idlewinn/collab-server/blob/master/src/index.ts
 
-import express from 'express';
-import http from 'http';
-import socketIO from 'socket.io';
+import express from "express";
+import http from "http";
+import socketIO from "socket.io";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hi, collab!');
+app.get("/", (req, res) => {
+  res.send("Hi, collab!");
 });
 
 const server = http.createServer(app);
@@ -20,48 +20,51 @@ server.listen(port, () => {
 const io = socketIO(server, {
   handlePreflightRequest: function(req, res) {
     var headers = {
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      'Access-Control-Allow-Origin': req.header ? req.header.origin : '*',
-      'Access-Control-Allow-Credentials': true,
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Origin": req.header ? req.header.origin : "*",
+      "Access-Control-Allow-Credentials": true
     };
     res.writeHead(200, headers);
     res.end();
-  },
+  }
 });
 
-io.on('connection', socket => {
-  console.log('connection established!');
-  io.to(`${socket.id}`).emit('init-room');
-  socket.on('join-room', roomID => {
+io.on("connection", socket => {
+  console.log("connection established!");
+  io.to(`${socket.id}`).emit("init-room");
+  socket.on("join-room", roomID => {
     console.log(`${socket.id} has joined ${roomID}`);
     socket.join(roomID);
     if (io.sockets.adapter.rooms[roomID].length <= 1) {
-      io.to(`${socket.id}`).emit('first-in-room');
+      io.to(`${socket.id}`).emit("first-in-room");
     } else {
-      socket.broadcast.to(roomID).emit('new-user', socket.id);
+      socket.broadcast.to(roomID).emit("new-user", socket.id);
     }
     io.in(roomID).emit(
-      'room-user-count',
-      io.sockets.adapter.rooms[roomID].length,
+      "room-user-count",
+      io.sockets.adapter.rooms[roomID].length
     );
   });
 
-  socket.on('server-broadcast', (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
-    console.log(`${socket.id} sends update to ${roomID}`);
-    socket.broadcast.to(roomID).emit('client-broadcast', encryptedData, iv);
-  });
+  socket.on(
+    "server-broadcast",
+    (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
+      console.log(`${socket.id} sends update to ${roomID}`);
+      socket.broadcast.to(roomID).emit("client-broadcast", encryptedData, iv);
+    }
+  );
 
-  socket.on('disconnecting', () => {
+  socket.on("disconnecting", () => {
     const rooms = io.sockets.adapter.rooms;
     for (const roomID in socket.rooms) {
       const remaining = rooms[roomID].length - 1;
       if (remaining > 0) {
-        socket.broadcast.to(roomID).emit('room-user-count', remaining);
+        socket.broadcast.to(roomID).emit("room-user-count", remaining);
       }
     }
   });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     socket.removeAllListeners();
   });
 });

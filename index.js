3b624150ab031e0233da842555ad5d42ef844410
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-03-09 20:20:38 +0200

    Initial version

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const server = app.listen(8080, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log(`Example app listening at http://${host}:${port}`);
+});

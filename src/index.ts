75a40efe28f5cd5c394bd9eacac3448777ef750f
Panayiotis Lipiridis <lipiridis@gmail.com>
2020-06-25 18:32:15 +0300

    Format

--- src/index.ts
+++ src/index.ts
@@ -17,21 +17,21 @@ server.listen(port, () => {
 });
 
 const io = socketIO(server, {
-  handlePreflightRequest: function(req, res) {
+  handlePreflightRequest: function (req, res) {
     var headers = {
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
       "Access-Control-Allow-Origin": req.header ? req.header.origin : "*",
-      "Access-Control-Allow-Credentials": true
+      "Access-Control-Allow-Credentials": true,
     };
     res.writeHead(200, headers);
     res.end();
-  }
+  },
 });
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
   ioDebug("connection established!");
   io.to(`${socket.id}`).emit("init-room");
-  socket.on("join-room", roomID => {
+  socket.on("join-room", (roomID) => {
     socketDebug(`${socket.id} has joined ${roomID}`);
     socket.join(roomID);
     if (io.sockets.adapter.rooms[roomID].length <= 1) {
@@ -67,7 +67,7 @@ io.on("connection", socket => {
     const rooms = io.sockets.adapter.rooms;
     for (const roomID in socket.rooms) {
       const clients = Object.keys(rooms[roomID].sockets).filter(
-        id => id !== socket.id
+        (id) => id !== socket.id
       );
       if (clients.length > 0) {
         socket.broadcast.to(roomID).emit("room-user-change", clients);

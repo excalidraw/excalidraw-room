d8351f1ee344a6b1973b4f6ed75df0ad0cdb6df9
Panayiotis Lipiridis <lipiridis@gmail.com>
2021-01-18 23:56:30 +0200

    remove console

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,6 @@ server.listen(port, () => {
 
 const io = socketIO(server, {
   handlePreflightRequest: function (req, res) {
-    console.log("req", req.header);
     var headers = {
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
       "Access-Control-Allow-Origin": "*",

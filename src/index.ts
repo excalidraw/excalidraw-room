5b174809e6deda842a572f1ab84a1a7f7c55805d
Panayiotis Lipiridis <lipiridis@gmail.com>
2021-02-07 13:47:01 +0200

    const

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ server.listen(port, () => {
 
 const io = socketIO(server, {
   handlePreflightRequest: function (req, res) {
-    var headers = {
+    const headers = {
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
       "Access-Control-Allow-Origin":
         (req.header && req.header.origin) || "https://excalidraw.com",

d48f62936060900489b95a50489f18adcbb6064c
Maielo <maielo.mv@gmail.com>
2023-05-16 09:49:29 +0200

    slight refactor of index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import debug from "debug";
 import express from "express";
 import http from "http";
-import socketIO from "socket.io";
+import { Server as SocketIO } from "socket.io";
 
 const serverDebug = debug("server");
 const ioDebug = debug("io");
@@ -28,67 +28,86 @@ server.listen(port, () => {
   serverDebug(`listening on port: ${port}`);
 });
 
-const io = socketIO(server, {
-  handlePreflightRequest: (req, res) => {
-    const headers = {
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      "Access-Control-Allow-Origin":
-        (req.header && req.header.origin) || "https://excalidraw.com",
-      "Access-Control-Allow-Credentials": true,
-    };
-    res.writeHead(200, headers);
-    res.end();
-  },
-});
-
-io.on("connection", (socket) => {
-  ioDebug("connection established!");
-  io.to(`${socket.id}`).emit("init-room");
-  socket.on("join-room", (roomID) => {
-    socketDebug(`${socket.id} has joined ${roomID}`);
-    socket.join(roomID);
-    if (io.sockets.adapter.rooms[roomID].length <= 1) {
-      io.to(`${socket.id}`).emit("first-in-room");
-    } else {
-      socket.broadcast.to(roomID).emit("new-user", socket.id);
-    }
-    io.in(roomID).emit(
-      "room-user-change",
-      Object.keys(io.sockets.adapter.rooms[roomID].sockets),
-    );
+try {
+  const io = new SocketIO(server, {
+    transports: ["websocket", "polling"],
+    cors: {
+      allowedHeaders: ["Content-Type", "Authorization"],
+      origin: process.env.CORS_ORIGIN || "*",
+      credentials: true,
+    },
+    allowEIO3: true,
+    // handlePreflightRequest: (req, res) => {
+    //   const headers = {
+    //     "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    //     "Access-Control-Allow-Origin":
+    //       (req.header && req.header.origin) || "https://excalidraw.com",
+    //     "Access-Control-Allow-Credentials": true,
+    //   };
+    //   res.writeHead(200, headers);
+    //   res.end();
+    // },
   });
 
-  socket.on(
-    "server-broadcast",
-    (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
-      socketDebug(`${socket.id} sends update to ${roomID}`);
-      socket.broadcast.to(roomID).emit("client-broadcast", encryptedData, iv);
-    },
-  );
-
-  socket.on(
-    "server-volatile-broadcast",
-    (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
-      socketDebug(`${socket.id} sends volatile update to ${roomID}`);
-      socket.volatile.broadcast
-        .to(roomID)
-        .emit("client-broadcast", encryptedData, iv);
-    },
-  );
-
-  socket.on("disconnecting", () => {
-    const rooms = io.sockets.adapter.rooms;
-    for (const roomID in socket.rooms) {
-      const clients = Object.keys(rooms[roomID].sockets).filter(
-        (id) => id !== socket.id,
-      );
-      if (clients.length > 0) {
-        socket.broadcast.to(roomID).emit("room-user-change", clients);
+  io.on("connection", (socket) => {
+    ioDebug("connection established!");
+    io.to(`${socket.id}`).emit("init-room");
+    socket.on("join-room", async (roomID) => {
+      socketDebug(`${socket.id} has joined ${roomID}`);
+      await socket.join(roomID);
+      const sockets = await io.in(roomID).fetchSockets();
+      if (sockets.length <= 1) {
+        io.to(`${socket.id}`).emit("first-in-room");
+      } else {
+        socketDebug(`${socket.id} new-user emitted to room ${roomID}`);
+        socket.broadcast.to(roomID).emit("new-user", socket.id);
       }
-    }
-  });
 
-  socket.on("disconnect", () => {
-    socket.removeAllListeners();
+      io.in(roomID).emit(
+        "room-user-change",
+        sockets.map((socket) => socket.id),
+      );
+    });
+
+    socket.on(
+      "server-broadcast",
+      (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
+        socketDebug(`${socket.id} sends update to ${roomID}`);
+        socket.broadcast.to(roomID).emit("client-broadcast", encryptedData, iv);
+      },
+    );
+
+    socket.on(
+      "server-volatile-broadcast",
+      (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
+        socketDebug(`${socket.id} sends volatile update to ${roomID}`);
+        socket.volatile.broadcast
+          .to(roomID)
+          .emit("client-broadcast", encryptedData, iv);
+      },
+    );
+
+    socket.on("disconnecting", async () => {
+      socketDebug(`${socket.id} has disconnected`);
+      for (const roomID in socket.rooms) {
+        const otherClients = (await io.in(roomID).fetchSockets()).filter(
+          (_socket) => _socket.id !== socket.id,
+        );
+
+        if (otherClients.length > 0) {
+          socket.broadcast.to(roomID).emit(
+            "room-user-change",
+            otherClients.map((socket) => socket.id),
+          );
+        }
+      }
+    });
+
+    socket.on("disconnect", () => {
+      socket.removeAllListeners();
+      socket.disconnect();
+    });
   });
-});
+} catch (error) {
+  console.error(error);
+}

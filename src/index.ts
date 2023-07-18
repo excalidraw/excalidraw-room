6b6015bab7fd175a631119741a5c61ed4f4a8a20
Astrid Doncker <astrid.doncker@qonfucius.team>
2023-07-18 16:35:59 +0200

    fix: lint

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,19 @@ import express from "express";
 import http from "http";
 import socketIO from "socket.io";
 // @ts-ignore
-import redisAdapter from 'socket.io-redis';
+import redisAdapter from "socket.io-redis";
 // @ts-ignore
-import redis from 'redis';
-import cors from 'cors';
+import redis from "redis";
+import cors from "cors";
 
 const serverDebug = debug("server");
 const ioDebug = debug("io");
 const socketDebug = debug("socket");
 
 require("dotenv").config(
-    process.env.NODE_ENV !== "development"
-        ? { path: ".env.production" }
-        : { path: ".env.development" },
+  process.env.NODE_ENV !== "development"
+      ? { path: ".env.production" }
+      : { path: ".env.development" },
 );
 
 const app = express();
@@ -25,9 +25,7 @@ app.use(cors());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.json({
-    msg: 'ok'
-  })
+  res.send("Excalidraw collaboration server is up :)");
 });
 
 app.get("/collab-server", (req, res) => {
@@ -44,7 +42,7 @@ const io = socketIO(server, {
     const headers = {
       "Access-Control-Allow-Headers": "Content-Type, Authorization",
       "Access-Control-Allow-Origin":
-          (req.header && req.header.origin) || "https://excalidraw.com",
+        (req.header && req.header.origin) || "https://excalidraw.com",
       "Access-Control-Allow-Credentials": true,
     };
     res.writeHead(200, headers);
@@ -57,7 +55,10 @@ server.listen(port, () => {
 });
 
 if (process.env.REDIS_HOST && process.env.REDIS_PORT) {
-  const pubClient = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+  const pubClient = redis.createClient(
+    process.env.REDIS_PORT,
+    process.env.REDIS_HOST,
+  );
   const subClient = pubClient.duplicate();
 
   io.adapter(redisAdapter({ pubClient, subClient }));
@@ -75,34 +76,34 @@ io.on("connection", (socket) => {
       socket.broadcast.to(roomID).emit("new-user", socket.id);
     }
     io.in(roomID).emit(
-        "room-user-change",
-        Object.keys(io.sockets.adapter.rooms[roomID].sockets),
+      "room-user-change",
+      Object.keys(io.sockets.adapter.rooms[roomID].sockets),
     );
   });
 
   socket.on(
-      "server-broadcast",
-      (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
-        socketDebug(`${socket.id} sends update to ${roomID}`);
-        socket.broadcast.to(roomID).emit("client-broadcast", encryptedData, iv);
-      },
+    "server-broadcast",
+    (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
+      socketDebug(`${socket.id} sends update to ${roomID}`);
+      socket.broadcast.to(roomID).emit("client-broadcast", encryptedData, iv);
+    },
   );
 
   socket.on(
-      "server-volatile-broadcast",
-      (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
-        socketDebug(`${socket.id} sends volatile update to ${roomID}`);
-        socket.volatile.broadcast
-            .to(roomID)
-            .emit("client-broadcast", encryptedData, iv);
-      },
+    "server-volatile-broadcast",
+    (roomID: string, encryptedData: ArrayBuffer, iv: Uint8Array) => {
+      socketDebug(`${socket.id} sends volatile update to ${roomID}`);
+      socket.volatile.broadcast
+        .to(roomID)
+        .emit("client-broadcast", encryptedData, iv);
+    },
   );
 
   socket.on("disconnecting", () => {
     const rooms = io.sockets.adapter.rooms;
     for (const roomID in socket.rooms) {
       const clients = Object.keys(rooms[roomID].sockets).filter(
-          (id) => id !== socket.id,
+        (id) => id !== socket.id,
       );
       if (clients.length > 0) {
         socket.broadcast.to(roomID).emit("room-user-change", clients);
